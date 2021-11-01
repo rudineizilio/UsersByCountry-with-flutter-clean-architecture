a93dca5b08a1c9805d311c237720bcdbe1e0e891
Rudinei Zilio <rudinei.zilio@MGA11045.local>
2021-11-01 09:23:41 -0300

    Implementa o DevicePreview

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,20 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(
+  DevicePreview(
+    enabled: !kReleaseMode,
+    builder: (context) => MyApp(),
+  ),
+);
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       title: 'Clean App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
